36f0f9c1703196fd1d7f20af653dc44d1091aba0
markus-demos-pluralsight <119988516+markus-demos-pluralsight@users.noreply.github.com>
2024-05-14 17:58:46 +0000

    demo

--- server/src/features/items/items.service.ts
+++ server/src/features/items/items.service.ts
@@ -1,41 +1,71 @@
 import { PrismaClient } from "@prisma/client";
 import { Item, ItemDTO, ItemDetail } from "../types";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import TTLCache from "@isaacs/ttlcache";
 
 const prisma = new PrismaClient();
+const options = { ttl: 1000 * 60 * 60 * 24 };
+const cache = new TTLCache(options);
+const listKey = "items-list";
 
 export function getItems(): Promise<Item[]> {
-  return prisma.item.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
+  const cacheItems = cache.get<Item[]>(listKey);
+  if (cacheItems != undefined) {
+    return Promise.resolve(cacheItems);
+  }
+
+  return prisma.item
+    .findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+    .then((items) => {
+      cache.set(listKey, items);
+      return items;
+    });
 }
 
 export function getItemDetail(itemId: number): Promise<ItemDetail | null> {
-  return prisma.item.findFirst({
-    where: { id: itemId },
-  });
+  const cacheItem = cache.get<ItemDetail>(itemId);
+  if (cacheItem != undefined) {
+    return Promise.resolve(cacheItem);
+  }
+
+  return prisma.item
+    .findFirst({
+      where: { id: itemId },
+    })
+    .then((item) => {
+      cache.set(itemId, item);
+      return item;
+    });
 }
 
 export function upsertItem(
   item: ItemDTO,
   itemId?: number | null
 ): Promise<Item | null> {
-  return prisma.item.upsert({
-    where: {
-      id: itemId || -1,
-    },
-    update: {
-      name: item.name,
-      description: item.description,
-    },
-    create: {
-      name: item.name,
-      description: item.description,
-    },
-  });
+  return prisma.item
+    .upsert({
+      where: {
+        id: itemId || -1,
+      },
+      update: {
+        name: item.name,
+        description: item.description,
+      },
+      create: {
+        name: item.name,
+        description: item.description,
+      },
+    })
+    .then((item) => {
+      cache.set(item.id, item);
+      cache.delete(listKey);
+      return item;
+    });
 }
 
 export function deleteItem(itemId: number): Promise<Item | null> {
@@ -52,5 +82,12 @@ export function deleteItem(itemId: number): Promise<Item | null> {
       } else {
         throw error;
       }
+    })
+    .then((item) => {
+      if (item != null) {
+        cache.delete(item.id);
+        cache.delete(listKey);
+      }
+      return item;
     });
 }
